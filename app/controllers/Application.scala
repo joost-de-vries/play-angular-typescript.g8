6602f07abd36e722996db130949a2cf582efc27f
Joost de Vries <joost@ziener.org>
2016-04-27 14:03:51 +0200

    adds comment

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,6 +6,11 @@ import play.api.mvc._
 class Application extends Controller {
 
   def index = Action {
+    /** change the template here to use a different way of compilation and loading of the ts ng2 app.
+      * index()  :    does no ts compilation in advance. the ts files are download by the browser and compiled there to js.
+      * index1() :    compiles the ts files to individual js files. Systemjs loads the individual files.
+      * index2() :    add the option -DtsCompileMode=stage to your sbt task . F.i. 'sbt ~run -DtsCompileMode=stage' this will produce the app as one single js file.
+      */
     Ok(views.html.index2())
   }
 
