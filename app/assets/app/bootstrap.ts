8ea5173eefad87db71060e25960c5a515a990039
Joost de Vries <joost@ziener.org>
2016-05-06 08:35:57 +0200

    small fix

--- app/assets/app/bootstrap.ts
+++ app/assets/app/bootstrap.ts
@@ -1,5 +1,5 @@
 import {bootstrap} from "angular2/platform/browser"
-import TodoApp from "./app"
+import TodoAppComponent from "./app"
 import {TodoStore} from "./services/store"
 
 bootstrap(TodoAppComponent, [TodoStore])
